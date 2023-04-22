ff3672a8c0db6ed7ca6c8c08877185074fcbbb02
Sabbadini10 <matiassabbadini24@gmail.com>
2023-04-22 16:01:51 -0300

    agrege nueva funcionalidad y di estilos a los botones

--- src/app/cards/cards/cards.component.ts
+++ src/app/cards/cards/cards.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class CardsComponent {
 
+  resultado : string = '';
+  datoFinal: string ="";
+
+
+  agregarNumero(num: number) {
+    this.resultado += num.toString();
+  }
+
+  agregarOperador(operator: string) {
+    this.resultado += ' ' + operator + ' ';
+  }
+
+  agregarMasMenos(operator: string) {
+      this.resultado += ' ' + operator + ' ';
+  }
+
+  calcular() {
+    // Verificar si el resultado actual es vacío
+    if (this.resultado === '') {
+      this.resultado = 'Error: Ingrese una operación válida';
+      return;
+    }
+
+    // Verificar si hay dos operadores consecutivos
+
+    const operadores = ['+', '-', '*', '/'];
+  for (let i = 0; i < operadores.length - 1; i++) {
+    const currentOp = operadores[i];
+    const nextOp = operadores[i + 1];
+    if (this.resultado.includes(nextOp) && this.resultado.endsWith(currentOp)) {
+      this.resultado = 'Error: No se pueden ingresar dos operadores consecutivos';
+      return;
+    }
+  }
+    // Si todo está bien, evaluar la operación
+    this.resultado = eval(this.resultado);
+    this.datoFinal = eval(this.resultado)
+  }
+
+
+
+
+  borrar() {
+    this.resultado = '';
+  }
+
+
 }
